ba49d0c6f8af79f8a033df8edcbac194103b8703
ASPshijiu <ASPshijiu@users.noreply.github.com>
2026-09-04 11:52:18 +0800

    build: fail fast when git metadata is unavailable

--- build/GitTasks.cs
+++ build/GitTasks.cs
@@ -5,8 +5,20 @@ static class GitTasks
 {
     public static string Git(this ICakeContext ctx, string args, string separator = "")
     {
-        using var process = ctx.StartAndReturnProcess("git", new() { Arguments = args, RedirectStandardOutput = true });
+        using var process = ctx.StartAndReturnProcess("git", new()
+        {
+            Arguments = args,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true
+        });
         process.WaitForExit();
+        var exitCode = process.GetExitCode();
+        if (exitCode != 0)
+        {
+            var error = string.Join("\n", process.GetStandardError());
+            throw new CakeException($"git {args} failed with exit code {exitCode}: {error}");
+        }
+
         return string.Join(separator, process.GetStandardOutput());
     }
 }
